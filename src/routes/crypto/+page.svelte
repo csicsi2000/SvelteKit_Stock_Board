3910f787de7cad4920b2e28f322855a4dab8eb27
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2023-01-13 17:38:25 +0100

    Add loading

--- src/routes/crypto/+page.svelte
+++ src/routes/crypto/+page.svelte
@@ -13,30 +13,38 @@
 </script>
 
 <div class="bg-dark bg-gradient">
-	<div class="p-5 table-responsive container">
-		<table class="table table-dark table-striped table-hover">
-			<thead>
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">Name</th>
-					<th class="text-center" scope="col">Symbol</th>
-					<th class="text-center" scope="col">USD</th>
-					<th class="text-center" scope="col">Change in 24h</th>
-					<th class="text-center" scope="col">Website</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each arrayOfCrypto as coin, i}
+	{#if arrayOfCrypto.length == 0}
+		<div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+			<div class="spinner-border text-info" role="status">
+				<span class="sr-only"></span>
+			</div>
+		</div>
+	{:else}
+		<div class="p-5 table-responsive container">
+			<table class="table table-dark table-striped table-hover">
+				<thead>
 					<tr>
-						<th scope="row">{i}</th>
-						<td><a href="/crypto/{coin.id}">{coin.id}</a></td>
-						<td class="text-center">{coin.symbol}</td>
-						<td class="text-center">{Math.round(+coin.priceUsd * 100) / 100}</td>
-						<td class="text-center">{Math.round(+coin.changePercent24Hr * 100) / 100} %</td>
-						<td class="text-center"><a href={coin.explorer} class="badge">{coin.explorer}</a></td>
+						<th scope="col">#</th>
+						<th scope="col">Name</th>
+						<th class="text-center" scope="col">Symbol</th>
+						<th class="text-center" scope="col">USD</th>
+						<th class="text-center" scope="col">Change in 24h</th>
+						<th class="text-center" scope="col">Website</th>
 					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</div>
+				</thead>
+				<tbody>
+					{#each arrayOfCrypto as coin, i}
+						<tr>
+							<th scope="row">{i}</th>
+							<td><a href="/crypto/{coin.id}">{coin.id}</a></td>
+							<td class="text-center">{coin.symbol}</td>
+							<td class="text-center">{Math.round(+coin.priceUsd * 100) / 100}</td>
+							<td class="text-center">{Math.round(+coin.changePercent24Hr * 100) / 100} %</td>
+							<td class="text-center"><a href={coin.explorer} class="badge">{coin.explorer}</a></td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	{/if}
 </div>
