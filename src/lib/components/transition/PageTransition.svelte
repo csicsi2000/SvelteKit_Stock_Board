dcfba5d4beb1b2950de2c09883f5f2119eda716d
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2023-01-13 18:58:38 +0100

    add transition in

--- src/lib/components/transition/PageTransition.svelte
+++ src/lib/components/transition/PageTransition.svelte
@@ -1,10 +1,11 @@
 <script>
 	import { fly } from 'svelte/transition';
+  import { page } from '$app/stores';
+
 </script>
 
-<div
-    in:fly="{{ y: -50, duration: 250, delay: 300 }}"
-    out:fly="{{ y: -50, duration: 250 }}" 
-    >
-    <slot/>
-  </div>
+{#key $page.route}
+	<div in:fly={{ x: -50, duration: 250, delay: 300 }} >
+		<slot />
+	</div>
+{/key}
